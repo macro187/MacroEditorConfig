48b5c10b5252563bcb03e084d6214d8e735bd239
Ron MacNeil <macro@hotmail.com>
2019-11-28 20:30:46 +1000

    Add EditorConfigLine.Index

--- MacroEditorConfig/EditorConfigLine.cs
+++ MacroEditorConfig/EditorConfigLine.cs
@@ -14,12 +14,29 @@ namespace MacroEditorConfig
                 throw new ArgumentOutOfRangeException(nameof(lineNumber), $"Invalid line number {lineNumber} specified");
             }
 
+            Index = lineNumber - 1;
             LineNumber = lineNumber;
             Text = text;
         }
 
 
+        /// <summary>
+        /// Zero-based ordinal position of the line within the file
+        /// </summary>
+        ///
+        public int Index { get; }
+
+
+        /// <summary>
+        /// One-based ordinal position of the line within the file
+        /// </summary>
         public int LineNumber { get; }
+
+
+        /// <summary>
+        /// The full text of the line
+        /// </summary>
+        ///
         public string Text { get; }
 
     }
